a53f27f99889466d4b6675b01fef909c28b716d6
Ahmed Bakir <ahmed@devatelier.com>
2015-08-16 23:58:13 -0700

    * added MagicalRecord

--- client/ios/InstaMelody/Pods/Target Support Files/Pods-InstaMelody/Pods-InstaMelody-frameworks.sh
+++ client/ios/InstaMelody/Pods/Target Support Files/Pods-InstaMelody/Pods-InstaMelody-frameworks.sh
@@ -53,7 +53,9 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework 'Pods-InstaMelody/FontAwesome_iOS.framework'
+  install_framework 'Pods-InstaMelody/MagicalRecord.framework'
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework 'Pods-InstaMelody/FontAwesome_iOS.framework'
+  install_framework 'Pods-InstaMelody/MagicalRecord.framework'
 fi
