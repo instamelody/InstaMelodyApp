3c6dfa5d495399a05bb1dd82609f74ed7f5ccf1a
Ahmed Bakir <ahmed@enthrallsports.com>
2015-10-16 07:39:48 -0700

    * Added notifications view

---
+++ client/ios/InstaMelody/InstaMelody/Controller/NotificationsViewController.h
@@ -0,0 +1,13 @@
+//
+//  NotificationsViewController.h
+//  InstaMelody
+//
+//  Created by Ahmed Bakir on 10/16/15.
+//  Copyright © 2015 InstaMelody. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotificationsViewController : UITableViewController
+
+@end
