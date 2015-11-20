bc6b057043b5db9360803441daeb3b61f1d59d22
Ahmed Bakir <ahmed@devatelier.com>
2015-11-19 23:52:45 -0500

    * Now showing complete details for a station

--- client/ios/InstaMelody/InstaMelody/Controller/StationViewController.m
+++ client/ios/InstaMelody/InstaMelody/Controller/StationViewController.m
@@ -14,6 +14,9 @@
 @interface StationViewController ()
 
 @property NSArray *loopArray;
+@property NSArray *cleanLoopArray;
+@property NSDateFormatter *fromDateFormatter;
+@property NSDateFormatter *toDateFormatter;
 
 @end
 
@@ -36,6 +39,19 @@
     //[self.vipButton setTitle:[NSString fontAwesomeIconStringForEnum:FAStar] forState:UIControlStateNormal];
     
     //self.liveImageView.hidden = ![[DataManager sharedManager] isPremium];
+    
+    self.fromDateFormatter = [[NSDateFormatter alloc] init];
+    
+    NSLocale *enUSPOSIXLocale = [NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"];
+    
+    [self.fromDateFormatter setLocale:enUSPOSIXLocale];
+    
+    [self.fromDateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss.SSS"];
+    [self.fromDateFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
+    
+    self.toDateFormatter = [[NSDateFormatter alloc] init];
+    [self.toDateFormatter setDateStyle:NSDateFormatterShortStyle];
+    [self.toDateFormatter setTimeStyle:NSDateFormatterShortStyle];
     
     [self getFirstStation];
     
@@ -168,7 +184,7 @@
             
             if (self.selectedFriend == nil) {
                 //get all my loops
-                [self fetchMyMelodies];
+                [self fetchMyLoops];
             } else {
                 //get my friend's public loops
             }
@@ -224,8 +240,49 @@
             //TODOAHMED
             //[alertView show];
             
-            [self fetchMyMelodies];
+            [self fetchMyLoops];
+            //[self fetchMyMelodies];
         }
+    }];
+}
+
+-(void)fetchMyLoops {
+    
+    NSString *requestUrl = [NSString stringWithFormat:@"%@/Melody/Loop", API_BASE_URL];
+    
+    NSString *token =  [[NSUserDefaults standardUserDefaults] objectForKey:@"authToken"];
+    
+    //add 64 char string
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    
+    NSDictionary *parameters = @{@"token": token};
+    
+    [manager GET:requestUrl parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        // NSLog(@"JSON: %@", responseObject);
+        NSLog(@"loop updated");
+        
+        NSArray *tempArray = (NSArray *)responseObject;
+        
+        //NSArray *tempArray = [[DataManager sharedManager] userMelodyList];
+        
+        if (tempArray.count > 20) {
+            tempArray = [tempArray subarrayWithRange:NSMakeRange(0, 20)];
+        }
+        
+        NSSortDescriptor *valueDescriptor = [[NSSortDescriptor alloc] initWithKey:@"DateCreated" ascending:NO];
+        NSArray *descriptors = [NSArray arrayWithObject:valueDescriptor];
+        self.loopArray = [tempArray sortedArrayUsingDescriptors:descriptors];
+        
+        self.cleanLoopArray = [tempArray sortedArrayUsingDescriptors:descriptors];
+        
+        [self.collectionView reloadData];
+        
+        
+        //NSDictionary *responseDict = (NSDictionary *)responseObject;
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Error fetching loops: %@", error);
+        
     }];
 }
 
@@ -239,7 +296,7 @@
         self.loopArray = [self.loopArray subarrayWithRange:NSMakeRange(0, 20)];
     }
     
-    NSSortDescriptor *valueDescriptor = [[NSSortDescriptor alloc] initWithKey:@"dateCreated" ascending:YES];
+    NSSortDescriptor *valueDescriptor = [[NSSortDescriptor alloc] initWithKey:@"dateCreated" ascending:NO];
     NSArray *descriptors = [NSArray arrayWithObject:valueDescriptor];
     self.loopArray = [tempArray sortedArrayUsingDescriptors:descriptors];
     
@@ -269,12 +326,16 @@
     StationCell *cell = [cv dequeueReusableCellWithReuseIdentifier:@"StationCell" forIndexPath:indexPath];
     //cell.backgroundColor = [UIColor whiteColor];
     
-    UserMelody *melody = (UserMelody *)[self.loopArray objectAtIndex:indexPath.row];
+    NSDictionary *loopDict = self.loopArray[indexPath.row];
     
     cell.shareButton.titleLabel.font  = [UIFont fontAwesomeFontOfSize:20.0f];
     cell.likeButton.titleLabel.font  = [UIFont fontAwesomeFontOfSize:20.0f];
-    cell.nameLabel.text = melody.userMelodyName;
-    cell.dateLabel.text = [self.dateFormatter stringFromDate:melody.dateCreated];
+    cell.nameLabel.text = [loopDict objectForKey:@"Name"];
+    
+    NSString *oldDateString = [loopDict objectForKey:@"DateCreated"];
+    NSDate *dateObject = [self.fromDateFormatter dateFromString:oldDateString];
+    
+    cell.dateLabel.text = [self.toDateFormatter stringFromDate:dateObject];
     
     
     [cell.shareButton setTitle:[NSString fontAwesomeIconStringForEnum:FAEllipsisH] forState:UIControlStateNormal];
@@ -293,7 +354,7 @@
     [cell.coverImage.layer insertSublayer:headerGradient atIndex:0];
      */
     
-    NSString *userId = melody.userId;
+    NSString *userId = [loopDict objectForKey:@"UserId"];
     
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     
@@ -303,11 +364,12 @@
 
         //if my station + public
         
+        /*
         if ([melody.isStationPostMelody boolValue] == YES) {
             [cell.joinButton setTitle:@"Public" forState:UIControlStateNormal];
         } else {
             [cell.joinButton setTitle:@"Private" forState:UIControlStateNormal];
-        }
+        }*/
         
         NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
         
@@ -350,19 +412,61 @@
 
 -(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
     
-    UserMelody *melody = (UserMelody *)[self.loopArray objectAtIndex:indexPath.row];
+    NSDictionary *loopDict = [self.loopArray objectAtIndex:indexPath.row];
     
-    if (melody != nil)  {
+    UIStoryboard *mainSB = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    
+    LoopViewController *loopVC = (LoopViewController *)[mainSB instantiateViewControllerWithIdentifier:@"LoopViewController"];
+    //loopVC.selectedUserMelody = melody;
+    loopVC.selectedLoop = loopDict;
+    
+    [self.navigationController pushViewController:loopVC animated:YES];
+}
+
+
+-(void)getLoop:(NSString *)loopId {
+    
+    NSString *requestUrl = [NSString stringWithFormat:@"%@/Melody/Loop", API_BASE_URL];
+    
+    NSString *token =  [[NSUserDefaults standardUserDefaults] objectForKey:@"authToken"];
+    
+    //add 64 char string
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    
+    NSDictionary *parameters = @{@"token": token, @"id": loopId};
+    
+    [manager GET:requestUrl parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"JSON: %@", responseObject);
         
-        UIStoryboard *mainSB = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-        
-        LoopViewController *loopVC = (LoopViewController *)[mainSB instantiateViewControllerWithIdentifier:@"LoopViewController"];
-        loopVC.selectedUserMelody = melody;
-        
-        [self.navigationController pushViewController:loopVC animated:YES];
-        
-        
-    }
+        if ([responseObject isKindOfClass:[NSDictionary class]]) {
+            NSDictionary *responseDict = (NSDictionary *)responseObject;
+            
+            
+            UIStoryboard *mainSB = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+            
+            LoopViewController *loopVC = (LoopViewController *)[mainSB instantiateViewControllerWithIdentifier:@"LoopViewController"];
+            //loopVC.selectedUserMelody = melody;
+            loopVC.loopDict = responseDict;
+            
+            [self.navigationController pushViewController:loopVC animated:YES];
+            
+            
+        }
+        //NSDictionary *responseDict = (NSDictionary *)responseObject;
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        if ([operation.responseObject isKindOfClass:[NSDictionary class]]) {
+            NSDictionary *errorDict = [NSJSONSerialization JSONObjectWithData:(NSData *)error.userInfo[AFNetworkingOperationFailingURLResponseDataErrorKey] options:0 error:nil];
+            
+            NSString *ErrorResponse = [NSString stringWithFormat:@"Error %ld: %@", operation.response.statusCode, [errorDict objectForKey:@"Message"]];
+            
+            NSLog(@"%@",ErrorResponse);
+            
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error" message:ErrorResponse delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [alertView show];
+        }
+    }];
+    
 }
 
 #pragma mark - Navigation
@@ -391,13 +495,13 @@
 -(IBAction)change:(id)sender {
     UISegmentedControl *control = (UISegmentedControl *)sender;
     
-    self.loopArray = [[DataManager sharedManager] userMelodyList];
+    self.loopArray = self.cleanLoopArray;
     
     switch (control.selectedSegmentIndex) {
         case 0: {
             
             NSArray *tempArray = self.loopArray;
-            NSSortDescriptor *valueDescriptor = [[NSSortDescriptor alloc] initWithKey:@"dateCreated" ascending:YES];
+            NSSortDescriptor *valueDescriptor = [[NSSortDescriptor alloc] initWithKey:@"DateCreated" ascending:NO];
             NSArray *descriptors = [NSArray arrayWithObject:valueDescriptor];
             self.loopArray = [tempArray sortedArrayUsingDescriptors:descriptors];
             
@@ -407,13 +511,15 @@
         case 1: {
             
             NSMutableArray *tempArray = [NSMutableArray new];
-            for (UserMelody *melody in self.loopArray) {
-                if (melody.parts.count > 1) {
-                    [tempArray addObject:melody];
+            
+            for (NSDictionary *itemDict in self.loopArray) {
+                NSArray *parts = [itemDict objectForKey:@"Parts"];
+                if (parts.count > 1) {
+                    [tempArray addObject:itemDict];
                 }
             }
             
-            NSSortDescriptor *valueDescriptor = [[NSSortDescriptor alloc] initWithKey:@"dateCreated" ascending:YES];
+            NSSortDescriptor *valueDescriptor = [[NSSortDescriptor alloc] initWithKey:@"DateCreated" ascending:NO];
             NSArray *descriptors = [NSArray arrayWithObject:valueDescriptor];
             self.loopArray = [tempArray sortedArrayUsingDescriptors:descriptors];
             
@@ -422,13 +528,14 @@
         }
         case 2: {
             NSMutableArray *tempArray = [NSMutableArray new];
-            for (UserMelody *melody in self.loopArray) {
-                if ([melody.isChatLoopPart boolValue] == TRUE) {
-                    [tempArray addObject:melody];
+            
+            for (NSDictionary *itemDict in self.loopArray) {
+                if ([[itemDict objectForKey:@"IsChatLoop"] boolValue] == TRUE) {
+                    [tempArray addObject:itemDict];
                 }
             }
             
-            NSSortDescriptor *valueDescriptor = [[NSSortDescriptor alloc] initWithKey:@"dateCreated" ascending:YES];
+            NSSortDescriptor *valueDescriptor = [[NSSortDescriptor alloc] initWithKey:@"DateCreated" ascending:NO];
             NSArray *descriptors = [NSArray arrayWithObject:valueDescriptor];
             self.loopArray = [tempArray sortedArrayUsingDescriptors:descriptors];
             
@@ -437,14 +544,15 @@
         case 3: {
             
             NSMutableArray *tempArray = [NSMutableArray new];
-            for (UserMelody *melody in self.loopArray) {
-                if (melody.parts.count == 1) {
-                    [tempArray addObject:melody];
+            for (NSDictionary *itemDict in self.loopArray) {
+                NSArray *parts = [itemDict objectForKey:@"Parts"];
+                if (parts.count == 1) {
+                    [tempArray addObject:itemDict];
                 }
             }
             
             
-            NSSortDescriptor *valueDescriptor = [[NSSortDescriptor alloc] initWithKey:@"dateCreated" ascending:YES];
+            NSSortDescriptor *valueDescriptor = [[NSSortDescriptor alloc] initWithKey:@"DateCreated" ascending:NO];
             NSArray *descriptors = [NSArray arrayWithObject:valueDescriptor];
             self.loopArray = [tempArray sortedArrayUsingDescriptors:descriptors];
             
